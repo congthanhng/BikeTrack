f848865c49febc3e1bd9a915d9b23a511c99facd
congnguyent <thanhcong.uit@gmail.com>
2022-03-30 15:12:29 +0700

    init pro

--- Components/CarItem/index.tsx
+++ Components/CarItem/index.tsx
@@ -1,6 +1,6 @@
-import { StatusBar, View, Text, SafeAreaView } from 'react-native'
- import styles from './styles'
+import {Text, SafeAreaView } from 'react-native'
 import tw from 'twrnc'
+import React from 'react';
 
 export default function CarItem() {
   return (
