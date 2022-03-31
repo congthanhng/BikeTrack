2fe148b3c25b45afbf098b574db72681c4bbee43
congnguyent <thanhcong.uit@gmail.com>
2022-03-31 17:08:06 +0700

    add sstate

--- src/CarItem/index.tsx
+++ src/CarItem/index.tsx
@@ -1,26 +1,42 @@
 import {Text, SafeAreaView } from 'react-native'
 import tw from 'twrnc'
-import React from 'react';
+import React , { useState } from 'react';
 import store from '../Redux/store';
 import { taskAdded, taskRemoved, taskCompleted } from '../Redux/actions';
 import { Provider } from 'react-redux';
 
 export default function CarItem() {
-  console.log('ok lets do it:');
-  const unsubcribe = store.subscribe(()=>{
-    console.log('store changed: ',store.getState());
-  })
+  // State: a counter value
+  const [counter, setCounter] = useState(0)
 
-  store.dispatch(taskAdded('first task'));
-  store.dispatch(taskAdded('second task'));
-  // store.dispatch(taskRemoved(1));
-  store.dispatch(taskCompleted(1));
+  // Action: code that causes an update to the state when something happens
+  const increment = () => {
+    setCounter(prevCounter => prevCounter + 1)
+  }
+
   return (
-    <Provider>
-      <SafeAreaView style={tw`bg-slate-600 h-full`}>
-        <Text style={tw`bg-blue-100 p-2`}>Hello, Cong Nguyen Thanhdd!</Text>
-      </SafeAreaView>
-    </Provider>
+    <div>
+      Value: {counter} <button onClick={increment}>Increment</button>
+    </div>
     
   )
 }
+
+// export default function CarItem() {
+//   console.log('ok lets do it:');
+//   const unsubcribe = store.subscribe(()=>{
+//     console.log('store changed: ',store.getState());
+//   })
+
+//   store.dispatch(taskAdded('first task'));
+//   store.dispatch(taskAdded('second task'));
+//   // store.dispatch(taskRemoved(1));
+//   store.dispatch(taskCompleted(1));
+//   return (
+    
+//       <SafeAreaView style={tw`bg-slate-600 h-full`}>
+//         <Text style={tw`bg-blue-100 p-2`}>Hello, Cong Nguyen Thanhdd!</Text>
+//       </SafeAreaView>
+    
+//   )
+// }
--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -1,13 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+// import { configureStore } from '@reduxjs/toolkit'
 
-export default configureStore({
-  reducer: {}
-})
+// export default configureStore({
+//   reducer: {}
+// })
 
 
-// import { createStore } from "redux";
-// import reducer from "./reducer";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-// const store = createStore(reducer);
+const store = createStore(reducer);
 
-// export default store;
+export default store;
